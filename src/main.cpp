921b364bc4bf9bba4ea71f0369dd3e83f10e4a75
Semi-Useful <justhdenow@gmail.com>
2026-03-16 18:15:06 -0600

    Update main.cpp
    ???

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,51 @@
 #include "hook.h"
 #include "converter.h"
+
 void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 {
-	switch (a_msg->type) {
-	case SKSE::MessagingInterface::kDataLoaded:
-		break;
-	case SKSE::MessagingInterface::kPostLoad:
-		break;
-	case SKSE::MessagingInterface::kPreLoadGame:
-		break;
-	case SKSE::MessagingInterface::kPostLoadGame:
-		if (APoseFix::Converter::HasErrors())
-		{
-			auto report = APoseFix::Converter::GetErrorReport();
-			RE::DebugMessageBox(report.c_str());
-			SKSE::log::error("{}", report);
-			APoseFix::Converter::ClearErrors();
-		}
+    switch (a_msg->type) {
+    case SKSE::MessagingInterface::kPostLoadGame:
+    case SKSE::MessagingInterface::kNewGame:
+        if (APoseFix::Converter::HasErrors())
+        {
+            auto report = APoseFix::Converter::GetErrorReport();
+            RE::DebugMessageBox(report.c_str());
+            SKSE::log::error("{}", report);
+            APoseFix::Converter::ClearErrors();
+        }
         break;
-	case SKSE::MessagingInterface::kNewGame:
-		if (APoseFix::Converter::HasErrors())
-		{
-			auto report = APoseFix::Converter::GetErrorReport();
-			RE::DebugMessageBox(report.c_str());
-			SKSE::log::error("{}", report);
-			APoseFix::Converter::ClearErrors();
-		}
-		break;
-	}
+    default:
+        break;
+    }
 }
 
 SKSEPluginLoad(const SKSE::LoadInterface* a_skse)
 {
-	SKSE::Init(a_skse);
+    SKSE::Init(a_skse);
+    SKSE::GetLogger()->set_level(spdlog::level::info);
+    SKSE::log::info("APoseFix starting load...");
 
-    auto messaging = SKSE::GetMessagingInterface();
-	if (!messaging->RegisterListener("SKSE", MessageHandler)) {
-		return false;
-	}
-	APoseFix::InstallHooks();
-	return true;
+    try {
+        auto messaging = SKSE::GetMessagingInterface();
+        if (!messaging->RegisterListener("SKSE", MessageHandler)) {
+            SKSE::log::error("Failed to register message listener");
+            return false;
+        }
+        SKSE::log::info("Registering message listener succeeded, installing hooks...");
+
+        APoseFix::InstallHooks();
+
+        SKSE::log::info("InstallHooks() completed");
+    }
+    catch (const std::exception& e) {
+        SKSE::log::error("Fatal C++ exception during load: {}", e.what());
+        return false;
+    }
+    catch (...) {
+        SKSE::log::error("Fatal unknown exception during load");
+        return false;
+    }
+
+    SKSE::log::info("APoseFix loaded successfully");
+    return true;
 }
